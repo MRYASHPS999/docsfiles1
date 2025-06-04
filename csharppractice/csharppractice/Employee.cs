ee5981dc4a435f772c04bcbfccb49dd9f78d280e
MR. YASH SAWANT <90855311+MRYASHPS999@users.noreply.github.com>
2025-06-04 12:27:21 +0530

    Add files via upload

---
+++ csharppractice/csharppractice/Employee.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+class Employee
+{
+    public static void display()
+    {
+        Console.WriteLine("This is static Method");
+    }
+
+    public void display2()
+    {
+        Console.WriteLine("This is Non-Static Method");
+    }
+
+    public string disName(string name)
+    {
+        return name;
+    }
+
+    public double Addval(int a, int b)
+    {
+        double c = a + b;
+        return c;
+    }
+
+
+}
---
+++ csharppractice/csharppractice/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+
+{
+    static void Main(string[]args)
+    {
+
+        Employee.display(); //calling static method
+
+        Employee emp = new Employee(); //object creation
+
+        emp.display2(); //non static method calling
+
+        string name1 = emp.disName("Jayesh");
+        Console.WriteLine(name1);
+
+
+        double dbl1 = emp.Addval(34, 567);
+        Console.WriteLine(dbl1);
+
+        //int id;
+        //string name;
+        //double salary;
+
+        //Console.WriteLine("Enter Name:");
+        //name=Console.ReadLine();
+
+        //Console.WriteLine("Enter Id:");
+        //id = int.Parse(Console.ReadLine());
+
+        //Console.WriteLine("Enter Salary:");
+        //salary = double.Parse(Console.ReadLine());
+
+        //string empdet = $"ID is {id}, Name is {name}, Salary is {salary}";
+        //Console.WriteLine(empdet);
+
+    }
+}
